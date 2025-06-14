0efa527228e3c3bfcb35b2beda30222a865ae263
@harhar25 <harold.jey.madjos@aclcbutuan.edu.ph>
2025-06-15 07:45:47 +0800

    Activity 2: Added About page

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return "<h1>Welcome to Activity 1 - Home Page</h1>"
 
+@app.route('/about')
+def about():
+    return "<h2>This is the About page for Activity 2</h2>"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
